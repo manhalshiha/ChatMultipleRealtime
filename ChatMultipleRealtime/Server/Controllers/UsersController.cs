cde6e042f9ffd2ea9eb8aab0555a28bac92b6007
manhal shiha <manhal6@gmail.com>
2023-12-30 07:38:38 +0300

    view all users underlie in our system in users list

---
+++ ChatMultipleRealtime/Server/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using ChatMultipleRealtime.Server.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatMultipleRealtime.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : BaseController
+    {
+        private readonly ChatContext chatContext;
+
+        public UsersController(ChatContext chatContext )
+        {
+            this.chatContext = chatContext;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<UserDto>> GetUsers()=> await chatContext.Users.AsNoTracking().Where(u=>u.Id!=UserId).Select(u => new UserDto(u.Id, u.Name)).ToListAsync();
+        
+    }
+}
